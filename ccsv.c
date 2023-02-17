4f35d66835f37c277dff4a979847c918a658723c
James Halim <tirtajames45@gmail.com>
2023-02-17 22:05:21 +0700

    newest

--- ccsv.c
+++ ccsv.c
@@ -45,33 +45,37 @@ void dataFree(struct Data *data)
 
 int dataInit(struct Data *data, size_t keysQ, size_t recordsQ, size_t valuesQ)
 {
-	if ((data->keys = malloc(keysQ * sizeof(struct Key))));
-	else goto ERROR;
-	for (data->keysQ = 0; data->keysQ < keysQ; ++data->keysQ) {
-		if ((data->keys[data->keysQ].key = malloc(MIN_STR_LEN)));
-		else goto ERROR;
-		data->keys[data->keysQ].keySize = MIN_STR_LEN;
-		data->keys[data->keysQ].keyLen = 0;
-	}
+	data->keysQ = 0;
+	data->recordsQ = 0;
 	if ((data->records = malloc(recordsQ * sizeof(struct Record))));
 	else
 		goto ERROR;
-	for (data->recordsQ = 0 ; data->recordsQ < recordsQ; ++data->recordsQ) {
+	data->records->valuesQ = 0;
+	if ((data->keys = malloc(keysQ * sizeof(struct Key))));
+	else goto ERROR_FREE;
+	for ( ; data->keysQ < keysQ; ++data->keysQ) {
+		if ((data->keys[data->keysQ].key = malloc(MIN_STR_LEN)));
+		else goto ERROR_FREE;
+		data->keys[data->keysQ].keySize = MIN_STR_LEN;
+		data->keys[data->keysQ].keyLen = 0;
+	}
+	for ( ; data->recordsQ < recordsQ; ++data->recordsQ) {
 		if ((data->records[data->recordsQ].values = malloc(valuesQ * sizeof(struct Value))));
-		else goto ERROR;
+		else goto ERROR_FREE;
 		data->records[data->recordsQ].valuesQ = valuesQ;
-		for (data->records->valuesQ = 0 ; data->records->valuesQ < valuesQ; ++data->records->valuesQ) {
+		for ( ; data->records->valuesQ < valuesQ; ++data->records->valuesQ) {
 			if ((data->records[data->recordsQ].values[data->records->valuesQ].value = malloc(MIN_STR_LEN)));
-			else goto ERROR;
+			else goto ERROR_FREE;
 			data->records[data->recordsQ].values[data->records->valuesQ].valueSize = MIN_STR_LEN;
 			data->records[data->recordsQ].values[data->records->valuesQ].valueLen = 0;
 		}
 	}
 	return 1;
 
+ERROR_FREE:
+	dataFree(data);
 ERROR:
 	perror("");
-	dataFree(data);
 	return 0;
 }
 
@@ -79,4 +83,5 @@ ERROR:
 int main()
 {
 	struct Data data;
+	dataInit(&data, 10, 10, 10);
 }
